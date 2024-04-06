d48f2d550fedd9813a465d82288d1e9a2d1909cc
Saketh Pallempati <90235687+SAKETH-PALLEMPATI@users.noreply.github.com>
2024-04-06 17:26:03 +0530

    v2

--- app.js
+++ app.js
@@ -2,80 +2,62 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const request = require("request");
-const initialData = require("./ServerStaticData/initialFetch.json");
-const sem = require("./ServerStaticData/sem.json");
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
-app.get("/api/getViews/:test_id", async (req, res) => {
-  let test_id = req.params.test_id;
-  request.get(
-    {
-      url: `https://api.api-ninjas.com/v1/counter?id=id${test_id}&hit=true`,
-      headers: {
-        "X-Api-Key": process.env.API,
-      },
-    },
-    function (error, response, body) {
-      if (error) return console.error("Request failed:", error);
-      else if (response.statusCode != 200)
-        return console.error(
-          "Error:",
-          response.statusCode,
-          body.toString("utf8")
-        );
-      else {
-        const ans = JSON.parse(body);
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.send(ans.value.toString());
-      }
-    }
-  );
-});
-
-app.get("/branch", async (req, res) => {
-  let branch = req.query.branch;
-  branch = "B.Tech. " + branch;
-  let ans = await Branch(branch);
-  res.json(ans);
-});
-app.get("/initialData", async (req, res) => {
-  // let output = await initialFetch().catch(console.dir);
-  res.json(initialData);
-});
-
-app.get("/sort", async (req, res) => {
-  let semno = req.query.sem;
-  // let output = await sortedOrder(sem).catch(console.dir);
-  students.find({});
-  res.json(sem[semno]);
-});
-
-app.get("/:id", async (req, res) => {
-  let sno = req.params.id;
-  let output = await run(sno).catch(console.dir);
-  res.json(output);
-});
+const axios = require("axios");
 
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGO;
 const client = new MongoClient(uri);
-const database = client.db("Project");
+const database = client.db("calicxData");
 const students = database.collection("students");
-async function run(sno) {
+
+app.get("/initialData", async (req, res) => {
+  let output = await initialFetch().catch(console.dir);
+  res.json(output);
+});
+
+app.get("/sort", async (req, res) => {
+  let semno = req.query.sem;
+  let output = await sortedOrder(semno).catch(console.dir);
+  res.json(output);
+});
+app.get("/branch", async (req, res) => {
+  let branch = req.query.branch;
+  branch = "B.Tech. " + branch;
+  let ans = await sortBranch(branch);
+  res.json(ans);
+});
+app.get("/:id", async (req, res) => {
+  let sno = req.params.id;
+  let output = await findDetails(sno).catch(console.dir);
+  res.json(output);
+});
+
+app.get("/api/getViews/:test_id", async (req, res) => {
+  let test_id = req.params.test_id;
+  try {
+    const response = await axios.get(
+      `https://api.api-ninjas.com/v1/counter?id=id${test_id}&hit=true`,
+      {
+        headers: {
+          "X-Api-Key": process.env.API,
+        },
+      }
+    );
+    const ans = response.data;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(ans.value.toString());
+  } catch (error) {
+    console.error("Request failed:", error);
+  }
+});
+
+async function findDetails(sno) {
   const filter = { SNo: parseInt(sno) };
   return await students.findOne(filter);
 }
-
-async function Branch(branch) {
+async function sortBranch(branch) {
   try {
     return await students
       .find({ Branch: branch.toString() })
@@ -94,33 +76,22 @@ async function Branch(branch) {
     console.error(error);
   }
 }
+function sortedOrder(semno) {
+  const query = `{"gpa.${semno}" : -1}`;
+  return students
+    .find()
+    .project({ _id: 0, RegNo: 1, Name: 1, Branch: 1, gpa: 1, rank: 1, SNo: 1 })
+    .sort(JSON.parse(query))
+    .limit(10)
+    .toArray();
+}
+async function initialFetch() {
+  return await students
+    .find()
+    .project({ _id: 0, RegNo: 1, Name: 1, SNo: 1 })
+    .toArray();
+}
 
-// return students
-//   .find({ "Branch": branch })
-//   .project({
-//     _id: 0,
-//     RegNo: 1,
-//     Name: 1,
-//     Branch: 1,
-//     gpa: 1,
-//     rank: 1,
-//     SNo: 1,
-//   })
-//   .sort({ rank: 1 });
-//   const query = `{"gpa.${sem}" : -1}`;
-//   return students
-//     .find()
-//     .project({ _id: 0, RegNo: 1, Name: 1, Branch: 1, gpa: 1, rank: 1, SNo: 1 })
-//     .sort(JSON.parse(query))
-//     .limit(10)
-//     .toArray();
-// }
-// async function initialFetch() {
-//   return await students
-//     .find()
-//     .project({ _id: 0, RegNo: 1, Name: 1, SNo: 1 })
-//     .toArray();
-// }
 app.listen(process.env.PORT, () =>
   console.log("Server started at " + process.env.PORT)
 );
--- temp.js
+++
@@ -1,9 +0,0 @@
-const x = ['B.Tech. Computer Science & Business Systems',
-    'B.Tech. Computer Science & Engineering',
-    'B.Tech. Computer Science & Engineering (Artificial Intelligence & Data Science)',
-    'B.Tech. Computer Science & Engineering (Cyber Security & Blockchain Technology)',
-    'B.Tech. Computer Science & Engineering (IoT & Automation)',
-    'B.Tech. Information & Communication Technology',
-    'B.Tech. Information Technology']
-ans = x.map((i) => i.replace("B.Tech. ", ""))
-console.log(ans);
